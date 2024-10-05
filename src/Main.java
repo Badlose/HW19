d2c3ad1a545fac7c8d1282438a7e245d998f559d
Badlose <git@github.com:Badlose/helloworld.git>
2024-10-05 14:13:17 +0300

    I`ve done all the tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String Log = "Testlog__123";
+        String Pass = "TestPass_123";
+        String passConfirm = "TestPass_123";
+
+        check(Log, Pass, passConfirm);
+
+    }
+
+    public static void check(String login, String password, String confirmPassword) {
+        Check check = new Check();
+        try {
+            check.check(login, password, confirmPassword);
+        } catch (WrongLoginException wrongLoginException) {
+            System.out.println("Введен некорректный логин");
+        } catch (WrongPasswordException wrongPasswordException) {
+            System.out.println("Введен некорректный пароль");
+        } finally {
+            System.out.println("Проверка завершена");
+        }
     }
 }
